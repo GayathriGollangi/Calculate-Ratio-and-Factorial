10f8abb4187bfd6e6f67e0169b9fac3bf41a8dcc
Gayathri <gayathrigollangii@gmail.com>
2022-10-18 13:56:30 +0000

    Calculate Ratio and Factorial

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,8 @@
+const ratioOfTwoNumbers = require("../ratio/index");
+const factorialOfNumber = require("../factorial/index");
+
+function ratioAndFactorial() {
+  console.log(factorial(5));
+  console.log(ratio(10, 2));
+}
+module.exports = ratioAndFactorial;
